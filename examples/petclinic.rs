efb3cff8b445bc5397bafc87f6fd56a76245f596
Haizea <haizea.cabrera.g@gmail.com>
2026-05-15 17:40:55 +0200

    chore(examples): remove verbose pipeline debug output

--- examples/petclinic.rs
+++ examples/petclinic.rs
@@ -73,15 +73,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     };
 
-    println!("Pipeline: {}", pl.name().unwrap_or_default());
-    println!("Stages: {}", pl.stages.len());
-    for (i, item) in pl.stages.iter().enumerate() {
-        match item {
-            pipeliner_core::pipeline::StageOrParallel::Stage(s) => println!("  {}. {} ({} steps)", i + 1, s.name, s.steps.len()),
-            pipeliner_core::pipeline::StageOrParallel::Parallel(g) => println!("  {}. parallel ({} stages)", i + 1, g.stages.len()),
-        }
-    }
-    
     // Run the pipeline
     let mut runner = PipelineRunner::new();
     println!("\n--- Running Pipeline ---");
